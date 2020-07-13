9d45d3b3c598b7f132c6a5776f05b1f70581c8ba
Hellom Oliveira <hellom-mendes@hotmail.com>
2020-07-13 20:43:34 +0100

    Desafio-4.2

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./rotas')
+const server = express()
+
+server.use(express.static('public'))
+server.use(routes)
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true,
+    watch: true 
+})
+
+
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
